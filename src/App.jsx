e40c606b184ba51c2161407689f71ecbcba4e064
Ian Valentino <ianrequena33@gmail.com>
2023-04-06 18:09:23 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 const App = () => {
   return (
     <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat">
+     
     <header className="container mx-auto px-4">
+    <div className="flex justify-between">
     <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
       <button>
         luna
       </button>
-
-    </header>
-     <form className="flex items-center gap-4 container mx-auto py-4 px-4 bg-white overflow-hidden rounded-md">
-     <span className="inline-block h-5 w-5 rounded-full border-2 mt-8"></span>
+      </div>
+    <form className="mt-8 flex items-center gap-4 container mx-auto py-4 px-4 bg-white overflow-hidden rounded-md">
+     <span className="inline-block h-5 w-5 rounded-full border-2"></span>
       <input className="w-full text-gray-400" type="text" placeholder="create a new todo"></input>
      </form>
-    <main className="container mx-auto px-4">
+     </header>
+
+     
+    <main className="container mx-auto px-4 mt-8">
       <article>
         <span>circulo</span>
         <p>complete javascript...</p>
@@ -39,7 +43,11 @@ const App = () => {
       <button>complete</button>
     </section>
     <p>Drag and drop to reorder list</p>
+   
+
+
     </div>
+     
   
   );
 };
