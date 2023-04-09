6f87fc3948d7ff74be577005c60a999b2ea72b53
Ian Valentino <ianrequena33@gmail.com>
2023-04-09 20:59:29 +0200

    -

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,16 @@
+import IconCheck from "./IconCheck";
 import CrossIcon from "./icons/CrossIcon";
 
 const TodoItem = ({todo}) => {
     return (
         <article className="flex gap-4  border-b border-b-gray-300">
-        <button className="border-b border-b-gray-30 flex-none inline-block h-5 w-5 rounded-full"></button>
+       {/*  <button className="flex h-5 w-5 border-b border-b-gray-30 flex-none rounded-full">
+        </button> */}
+
+        <button className="flex h-5 w-5 border-b border-b-gray-30 flex-none rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 justify-center items-center">
+            <IconCheck />
+        </button>
+
         <p className="grow text-gray-600">{todo.title}</p>
         <button>
         <CrossIcon />
